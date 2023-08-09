35adddb2a56185daab40be2322cc0610a829e1c9
amoredisperato <141871974+amoredisperato@users.noreply.github.com>
2023-08-09 22:07:56 +0200

    Corrected fetch address of XML file

--- js/calendar.js
+++ js/calendar.js
@@ -264,7 +264,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     async function getConfigFile() {
-        const fileAddress = 'https://amoredisperato.github.io/calendar.xml';
+        const fileAddress = 'https://amoredisperato.github.io/config/calendar.xml';
         const requestInit = {
             'headers': {
                 'accept': 'application/xml'
